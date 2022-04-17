729a07314483e7923c9718aa054d28df402ab234
mahfuzswaron <mahfuzswaron@gmail.com>
2022-04-17 13:05:26 +0600

    navbar done

--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container, Nav, Navbar} from 'react-bootstrap';
+import {Link} from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is Header</h1>
-        </div>
+    <Navbar bg="primary" variant="dark">
+    <Container>
+        <Navbar.Brand as={Link} to="/">DOCK ONLINE</Navbar.Brand>
+        <Nav className="me-auto">
+        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+        <Nav.Link as={Link} to="/checkout">Check Out</Nav.Link>
+        <Nav.Link as={Link} to="/login">Login</Nav.Link>
+        </Nav>
+    </Container>
+  </Navbar>
+
     );
 };
 
