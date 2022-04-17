afbe73d393d6b4c6f70ea392b10d71cf4376be43
mahfuzswaron <mahfuzswaron@gmail.com>
2022-04-17 22:45:12 +0600

    login with email and password done

--- src/components/shared/Login/Login.js
+++ src/components/shared/Login/Login.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../shared/Firebase/Firebase.init';
 
 const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+      if(error ){
+        console.log('error from hook', error.message);
+        return <p>invalid email or password</p>
+    }
+    if(loading){
+        return <p>Loading...</p>
+    }
+    if(user){
+        console.log(user);
+        return <p>{user.user.displayName} is regestered</p>
+    }
+    const handleSignIn = (e) =>{
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+        signInWithEmailAndPassword(email, password)
+
+    }
+
     return (
         <div>
+            <h2 className='text-primary text-center my-5'>Sign up</h2>
+            <div className='d-flex justify-content-center mx-auto'>
+            <Form onSubmit={(e)=> handleSignIn(e)}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" name='checkbox' label="Accept terms and conditions" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Log in
+                </Button>
+            </Form>
+            </div>
+            <p className='text-center'>New here? <Link className='text-decoration-none' to='/signup'>Sign up here</Link> </p>
             
         </div>
     );
