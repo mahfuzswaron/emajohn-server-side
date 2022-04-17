272f299e20b5bc44e389fa11f6783764a19e23cd
mahfuzswaron <mahfuzswaron@gmail.com>
2022-04-17 14:42:55 +0600

    banner added

--- src/components/pages/Home/Home/Home.js
+++ src/components/pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import banner from '../../../../images/banner.png'
 
 const Home = () => {
     return (
         <div>
-            
+            <div className='d-flex justify-content-center '>
+             <img src={banner} alt='banner' />
+            </div>
         </div>
     );
 };
